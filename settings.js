00d198f386cebe6217ddff7bc400bdf1548bd623
Mrmaterial <150019108+Mrmaterial@users.noreply.github.com>
2025-12-15 06:47:27 -0600

    Update SESSION_ID and OWNER_NUMBER in settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 
 module.exports = {
-  SESSION_ID: "",  // add session Id here
+  SESSION_ID: "IK~bB1xjayb#A6sT4Q7JLuBid5qsqaPAaGN6R0YzdpmVPSTs9Lor1NA",  // add session Id here
   
-  OWNER_NUMBER: "923427582273", // add owner number 
+  OWNER_NUMBER: "255679695311", // add owner number 
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "" // put your country timeZone....leave blank if u don't know.
 };
 
